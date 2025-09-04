c892d02c966af9918151c3f9dd5ee5f5936051c2
JottenJ <otto.jalo@edu.prakticum.fi>
2025-09-04 11:55:41 +0300

    Update Otto lucky med counter.py

--- Otto lucky med counter.py
+++ Otto lucky med counter.py
@@ -1,12 +1,22 @@
-print("Enter a lucky number")
+import random
+
+
 def fav():
-    guesscount = 0
+    luckynum = random.randint(1, 10)
+    guesscount = 1
+    print("Enter a lucky number between 1 and 10")
     while True:
-        num = int(input("What is the lucky number?: "))
-        if num == 5:
+        num_str = input("What is the lucky number?: ")
+        if not num_str.isdigit():
+            print("håll nu fingrarna bara på siffrorna, tack!")
+            continue
+        num = int(num_str)
+        if num == luckynum:
             print(f"Grattis! Du gissade rätt i {guesscount} försök")
+            break
         else:
             print("Fel gissat, försök igen!")
             guesscount += 1
+
 
 fav()
